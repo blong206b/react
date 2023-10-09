fc39be32ecfee13f46d2a78b3d70d914acaaccc1
blong206b <blong206b@gmail.com>
2023-10-09 07:12:47 -0400

    changges

--- WYWM/app.js
+++ WYWM/app.js
@@ -1,44 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Your code will go here
-    
-    // Feature 1: Display Current Weather
-    document.getElementById('current-weather-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        const location = document.getElementById('current-weather-location').value;
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('current-weather-result').innerText = `Temperature: ${data.main.temp}°C`;
-        });
-    });
-
-    // Feature 2: 5-Day Weather Forecast
-    document.getElementById('five-day-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        const location = document.getElementById('five-day-location').value;
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
-        .then(response => response.json())
-        .then(data => {
-            // Loop through the array and display the 5-day forecast.
-            // This part depends on how you want to format it.
-        });
-    });
-
-    // Feature 3: Activity Suggestions
-    document.getElementById('activity-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        const location = document.getElementById('activity-location').value;
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
-        .then(response => response.json())
-        .then(data => {
-            const temp = data.main.temp;
-            let suggestion = '';
-            if (temp > 25) {
-                suggestion = 'How about a trip to the park today?';
-            } else {
-                suggestion = 'Maybe stay inside and practice your coding exercises. After 2 hours, play a round of World of Warcraft?';
-            }
-            document.getElementById('activity-result').innerText = suggestion;
-        });
+// Feature 1: Current Weather
+document.getElementById('current-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const location = document.getElementById('current-location').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('current-result').innerText = `Temperature: ${data.main.temp}°C`;
     });
 });
+
+// Feature 2: 5-Day Weather Forecast
+document.getElementById('five-day-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const location = document.getElementById('five-day-location').value;
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+        let forecastText = '';
+        for (let i = 0; i < data.list.length; i+=8) {
+            forecastText += `Day ${i/8 + 1}: Temperature: ${data.list[i].main.temp}°C\n`;
+        }
+        document.getElementById('five-day-result').innerText = forecastText;
+    });
+});
+
+// Feature 3: Activity Suggestions
+document.getElementById('suggestions-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const location = document.getElementById('suggestions-location').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+        const temp = data.main.temp;
+        let suggestion = '';
+        if (temp > 20) {
+            suggestion = 'How about a trip to the park today?';
+        } else {
+            suggestion = 'Maybe stay inside and practice your coding exercises. After 2 hours, play a round of World of Warcraft?';
+        }
+        document.getElementById('suggestions-result').innerText = suggestion;
+    });
+});
+
