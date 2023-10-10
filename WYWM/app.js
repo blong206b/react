957b857a5c61f303fbb237d30c1762c4921582fd
blong206b <blong206b@gmail.com>
2023-10-09 21:10:35 -0400

    changes

--- WYWM/app.js
+++ WYWM/app.js
@@ -2,59 +2,60 @@
 document.getElementById('current-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const location = document.getElementById('current-location').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('current-result').innerText = `Temperature: ${data.main.temp}°C`;
-    });
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('current-result').innerText = `Temperature: ${data.main.temp}°F`;
+        });
 });
 
-// Feature 2: Temperature Compared to Sydney
+
+// Feature 2: Activity Suggestions
+document.getElementById('suggestions-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const location = document.getElementById('suggestions-location').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
+        .then(response => response.json())
+        .then(data => {
+            const temp = data.main.temp;
+            let suggestion = '';
+            if (temp > 59) {
+                suggestion = "It's Warm outside (60+), How about a trip to the park today?";
+            } else {
+                suggestion = "It's Cool outside (under 60), Maybe stay inside and practice your coding exercises.";
+            }
+
+            document.getElementById('suggestions-result').innerText = suggestion;
+        });
+});
+
+// Feature 3: Temperature Compared to Sydney
 document.getElementById('compare-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const location = document.getElementById('compare-location').value;
 
     // Fetch the temperature for the user-specified location
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-        const tempUserLocation = data.main.temp;
-
-        // Fetch the temperature for Sydney
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
         .then(response => response.json())
         .then(data => {
-            const tempSydney = data.main.temp;
+            const tempUserLocation = data.main.temp;
 
-            let comparisonResult = '';
-            if (tempUserLocation > tempSydney) {
-                comparisonResult = 'warmer';
-            } else if (tempUserLocation < tempSydney) {
-                comparisonResult = 'cooler';
-            } else {
-                comparisonResult = 'the same temperature as';
-            }
+            // Fetch the temperature for Sydney
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
+                .then(response => response.json())
+                .then(data => {
+                    const tempSydney = data.main.temp;
 
-            document.getElementById('compare-result').innerText = `It's ${comparisonResult} than Sydney.`;
+                    let comparisonResult = '';
+                    if (tempUserLocation > tempSydney) {
+                        comparisonResult = 'warmer';
+                    } else if (tempUserLocation < tempSydney) {
+                        comparisonResult = 'cooler';
+                    } else {
+                        comparisonResult = 'the same temperature as';
+                    }
+
+                    document.getElementById('compare-result').innerText = `It's ${comparisonResult} than Sydney.`;
+                });
         });
-    });
 });
-
-// Feature 3: Activity Suggestions
-document.getElementById('suggestions-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const location = document.getElementById('suggestions-location').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-        const temp = data.main.temp;
-        let suggestion = '';
-        if (temp > 20) {
-            suggestion = 'How about a trip to the park today?';
-        } else {
-            suggestion = 'Maybe stay inside and practice your coding exercises. After 2 hours, play a round of World of Warcraft?';
-        }
-        document.getElementById('suggestions-result').innerText = suggestion;
-    });
-});
-
