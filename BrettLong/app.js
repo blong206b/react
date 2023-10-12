7ee1bdffb5ea259ec87386980b93a9cd1ff3d38c
blong206b <blong206b@gmail.com>
2023-10-12 09:07:14 -0400

    weather changes

--- BrettLong/app.js
+++ BrettLong/app.js
@@ -1,37 +1,59 @@
-// Feature 1: Current Weather
+// Main JavaScript file for the Weather App
+
+// --- Feature 1: Current Weather ---
+// Attach a submit event listener to the "current-form" element
 document.getElementById('current-form').addEventListener('submit', function(e) {
+    // Prevent form submission
     e.preventDefault();
+
+    // Get the user's input location
     const location = document.getElementById('current-location').value;
+
+    // Fetch current weather information based on the user's location
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
         .then(response => response.json())
         .then(data => {
+            // Display current temperature
             document.getElementById('current-result').innerText = `Temperature: ${data.main.temp}°F`;
         });
 });
 
 
-// Feature 2: Activity Suggestions
+// --- Feature 2: Activity Suggestions ---
+// Attach a submit event listener to the "suggestions-form" element
 document.getElementById('suggestions-form').addEventListener('submit', function(e) {
+    // Prevent form submission
     e.preventDefault();
+
+    // Get the user's input location
     const location = document.getElementById('suggestions-location').value;
+
+    // Fetch current weather information based on the user's location
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
         .then(response => response.json())
         .then(data => {
             const temp = data.main.temp;
             let suggestion = '';
+
+            // Provide activity suggestions based on temperature
             if (temp > 59) {
                 suggestion = "It's Warm outside (60+), How about a trip to the park today?";
             } else {
                 suggestion = "It's Cool outside (under 60), Maybe stay inside and practice your coding exercises.";
             }
 
+            // Display the activity suggestion
             document.getElementById('suggestions-result').innerText = suggestion;
         });
 });
 
-// Feature 3: Temperature Compared to Sydney
+// --- Feature 3: Temperature Compared to Sydney ---
+// Attach a submit event listener to the "compare-form" element
 document.getElementById('compare-form').addEventListener('submit', function(e) {
+    // Prevent form submission
     e.preventDefault();
+
+    // Get the user's input location
     const location = document.getElementById('compare-location').value;
 
     // Fetch the temperature for the user-specified location
@@ -40,13 +62,14 @@ document.getElementById('compare-form').addEventListener('submit', function(e) {
         .then(data => {
             const tempUserLocation = data.main.temp;
 
-            // Fetch the temperature for Sydney
+            // Fetch the temperature for Sydney for comparison
             fetch(`https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=24306fad6bfddec85c08fc31487d2c8a&units=imperial`)
                 .then(response => response.json())
                 .then(data => {
                     const tempSydney = data.main.temp;
-
                     let comparisonResult = '';
+
+                    // Determine and display how the temperature compares to Sydney
                     if (tempUserLocation > tempSydney) {
                         comparisonResult = 'warmer';
                     } else if (tempUserLocation < tempSydney) {
@@ -55,6 +78,7 @@ document.getElementById('compare-form').addEventListener('submit', function(e) {
                         comparisonResult = 'the same temperature as';
                     }
 
+                    // Display the comparison result
                     document.getElementById('compare-result').innerText = `It's ${comparisonResult} than Sydney.`;
                 });
         });
